8ce434a30559db42321c45e0b86934d2fe1b8b37
Aadithye N <aadithyen@gmail.com>
2021-10-19 15:07:18 +0530

    Greeting for new user

--- bot.py
+++ bot.py
@@ -7,10 +7,29 @@ import random
 
 load_dotenv()
 
+intents = discord.Intents.default()
+intents.members = True
+intents.reactions = True
+client = discord.Client(intents=intents)
+
+greetings = [
+    "Hola there, ",
+    "Wilkkomen, ",
+    "Glad to see you, ",
+    "Nice to have you here, "
+]
+
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 
+@client.event
+async def on_member_join(member):
+    print('New user here')
+    channel = client.get_channel(899924394971893830)
+    greeting = random.choice(greetings) + member.display_name
+    await channel.send(greeting)
 
 client.run(os.environ.get('BOT_TOKEN'))
